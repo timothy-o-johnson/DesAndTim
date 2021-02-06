970ddf328731642ba09da907f9c5ec179d109f68
Tim Johnson <tjohnson@2ndswing.com>
2021-02-05 19:21:22 -0500

    import score-card-kata into test file

---
+++ ScoreCardKata/__tests__/ScoreCardKata.test.js
@@ -0,0 +1,5 @@
+const scoreCard = require('../score-card-kata')
+
+test(' 0. create a a class ScoreKeeper which offers following methods', () => {
+  console.log('scoreCard', scoreCard)
+})
---
+++ ScoreCardKata/score-card-kata.js
@@ -0,0 +1,3 @@
+const  scoreCard = 3
+ 
+module.exports = {scoreCard }
