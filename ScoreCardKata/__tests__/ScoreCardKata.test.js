0ccee60f18abaf557dc3b5a9805b4a759183550b
Tim Johnson <tjohnson@2ndswing.com>
2021-02-12 16:40:19 -0500

    0. create a class ScoreKeeper which offers following methods: scoreTeamA1(), scoreTeamA2(), scoreTeamA3(), scoreTeamB1(), scoreTeamB2(), scoreTeamB3(), getScore()

--- ScoreCardKata/__tests__/ScoreCardKata.test.js
+++ ScoreCardKata/__tests__/ScoreCardKata.test.js
@@ -1,5 +1,35 @@
 const scoreCard = require('../score-card-kata')
 
-test(' 0. create a a class ScoreKeeper which offers following methods', () => {
-  console.log('scoreCard', scoreCard)
+test(' 0. create a class ScoreKeeper which offers following methods: scoreTeamA1(), scoreTeamA2(), scoreTeamA3(), scoreTeamB1(), scoreTeamB2(), scoreTeamB3(), getScore()', () => {
+  // console.log('scoreCard', scoreCard)
+
+  var scoreCardTest = new scoreCard()
+
+  console.log('scoreCardTest', scoreCardTest)
+
+  expect(scoreCardTest.scoreTeamA1).toBeDefined()
+  expect(scoreCardTest.scoreTeamA2).toBeDefined()
+  expect(scoreCardTest.scoreTeamA3).toBeDefined()
+  expect(scoreCardTest.scoreTeamB1).toBeDefined()
+  expect(scoreCardTest.scoreTeamB2).toBeDefined()
+  expect(scoreCardTest.scoreTeamB3).toBeDefined()
+  expect(scoreCardTest.getScore).toBeDefined()
 })
+
+
+
+
+// class someClass {
+//   constructor() {
+   
+//   }
+
+//   helloWorld(){
+//     console.log('hello world!')
+    
+//   }
+// }
+
+// var someClass1 = new someClass()
+
+// someClass1.helloWorld()
--- ScoreCardKata/score-card-kata.js
+++ ScoreCardKata/score-card-kata.js
@@ -1,3 +1,18 @@
-const  scoreCard = 3
- 
-module.exports = {scoreCard }
+class scoreCard {
+  constructor () {}
+
+  scoreTeamA1 () {
+  }
+
+ scoreTeamA1() {}
+ scoreTeamA2() {}
+ scoreTeamA3() {}
+ scoreTeamB1() {}
+ scoreTeamB2() {}
+ scoreTeamB3() {}
+ getScore() {}
+
+
+}
+
+module.exports = scoreCard
