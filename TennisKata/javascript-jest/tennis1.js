a1dd6d8f42c0eea52e58d9e59d933e087e2551c1
Tim Johnson <tjohnson@2ndswing.com>
2021-03-18 20:38:33 -0400

    refactor
    Co-authored-by: dezcliett <dezcliett@users.noreply.github.com>

--- TennisKata/javascript-jest/tennis1.js
+++ TennisKata/javascript-jest/tennis1.js
@@ -1,77 +1,81 @@
-"use strict";
+'use strict'
 
-function getScore(player_score1, player_score2) {
-  var score = "";
-  var tempScore = 0;
+function getScore (player_score1, player_score2) {
+  var score = ''
+  var tempScore = 0
+
+  var p1ScoreText = takePointsAndConvertToText(player_score1) // 1pt -> 'Fifteen'
+  var p2ScoreText = takePointsAndConvertToText(player_score2) // 1pt -> 'Fifteen'
+
+  score = updateScoreboard(p1ScoreText, p2ScoreText) // 'Fifteen-Fifteen'
 
   // If score 1 & 2 are equal
   if (player_score1 === player_score2) {
     // This function shows all the possible names for a tie score
-    switch (player_score1) {
-      case 0:
-        score = "Love-All";
-        break;
-      case 1:
-        score = "Fifteen-All";
-        break;
-      case 2:
-        score = "Thirty-All";
-        break;
+    switch (p1ScoreText) {
+      case 'Love':
+        score = 'Love-All'
+        break
+      case 'Fifteen':
+        score = 'Fifteen-All'
+        break
+      case 'Thirty':
+        score = 'Thirty-All'
+        break
       default:
-        score = "Deuce";
-        break;
+        score = 'Deuce'
+        break
     }
   } else if (player_score1 >= 4 || player_score2 >= 4) {
     // If one or the other has a score higher or equal to 4
     // Find the difference between the 2 scores
-    var minusResult = player_score1 - player_score2;
+    var minusResult = player_score1 - player_score2
     // If Player 1 has 1 point lead then the score is "Advantage", if the lead is 2 points then player 1 win. Same for player 2 but with -1 and if its anything else, player 2 wins
     if (minusResult === 1) {
-      score = "Advantage player1";
+      score = 'Advantage player1'
     } else if (minusResult === -1) {
-      score = "Advantage player2";
+      score = 'Advantage player2'
     } else if (minusResult >= 2) {
-      score = "Win for player1";
+      score = 'Win for player1'
     } else {
-      score = "Win for player2";
+      score = 'Win for player2'
     }
   } // if not a tie and the players have less than 4 points... then build score
   else {
-    var p1ScoreText = takePointsAndConvertToText(player_score1); // 'Forty'
-    var p2ScoreText = takePointsAndConvertToText(player_score2); // 'Fifteen'
-
-    score = updateScoreboard(p1ScoreText, p2ScoreText);
+    
   }
-  return score;
+  
+  return score
 
-  function takePointsAndConvertToText(playerScore) {
-    var text;
+  function takePointsAndConvertToText (playerScore) {
+    var text
 
     switch (playerScore) {
       case 0:
-        text = "Love";
-        break;
+        text = 'Love'
+        break
 
       case 1:
-        text = "Fifteen";
-        break;
+        text = 'Fifteen'
+        break
 
       case 2:
-        text = "Thirty";
-        break;
+        text = 'Thirty'
+        break
 
       case 3:
-        text = "Forty";
-        break;
+        text = 'Forty'
+        break
     }
 
-    return text;
+    return text
   }
-  function updateScoreboard(p1ScoreText, p2ScoreText) {
+
+  function updateScoreboard (p1ScoreText, p2ScoreText) {
     // p1ScoreText = 'Forty',  p2ScoreText = 'Fifteen'
-    var score = p1ScoreText + "-" + p2ScoreText; // 'Forty-Fifteen'
-    return score;
+    var score = p1ScoreText + '-' + p2ScoreText // 'Forty-Fifteen'
+    return score
   }
 }
 
-module.exports = getScore;
+module.exports = getScore
